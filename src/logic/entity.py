97dfaa1457649f404e15de91afa4000464197f45
Marquis Kurt <software@marquiskurt.net>
2021-02-26 15:20:57 -0500

    ✨ Create a non-player entity

---
+++ src/logic/entity.py
@@ -0,0 +1,43 @@
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+
+from math import sqrt
+from random import randint
+from typing import Tuple
+from src.logic import Player
+
+
+class NonPlayerEntity():
+    """A non-player entity that a player can interact with to attempt to get into a relationship with."""
+
+    def __init__(self, position: Tuple[int, int]) -> None:
+        """Create an entity.
+
+        Arguments:
+            position (tuple): The entity's position on the map.
+        """
+        self.position = position
+        self.max_love_level = float(randint(1, 10))
+        self.current_love_level = 0.0
+
+    def is_near(self, player: Player) -> bool:
+        """Returns whether the entity is near a player in the world."""
+        ex, ey = self.position
+        px, py = player.position
+
+        distance = sqrt(pow(px - ex, 2) + pow(py - ey, 2))
+        return distance < 48
+
+    def transfer(self, amount: float):
+        """Transfers the love amount from one source to another."""
+        if self.current_love_level + amount > self.max_love_level:
+            self.current_love_level = self.max_love_level
+            return
+        self.current_love_level += amount
+
+    def verify(self) -> bool:
+        """Returns whether the entity accepts/rejects the relationship."""
+        return self.current_love_level == self.max_love_level and randint(1, 20) > 15
