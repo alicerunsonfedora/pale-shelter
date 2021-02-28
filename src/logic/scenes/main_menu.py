440233e533443178f154ac148066fcdee11c244f
Marquis Kurt <software@marquiskurt.net>
2021-02-28 12:34:12 -0500

    💄 Add cheatsheets to main menu

--- src/logic/scenes/main_menu.py
+++ src/logic/scenes/main_menu.py
@@ -22,6 +22,8 @@ class MainMenu(scene.GameScene):
         self.regular_font = pygame.font.Font(
             asset_path("assets/fonts/Forum-Regular.ttf"), 32)
 
+        self._htp_size = 192, 256
+
         self.title_text = self.title_font.render(
             "NO LOVE", True, self.palette.get_color("TITLE_COLOR"))
         self.start_button = self.regular_font.render(
@@ -30,6 +32,11 @@ class MainMenu(scene.GameScene):
             "QUIT", True, (255, 255, 255))
         self.start_rect = self.start_button.get_rect()
         self.quit_rect = self.quit_button.get_rect()
+
+        self.how_to_play_images = {
+            "left": pygame.image.load(asset_path("assets/ui/htp_1.png")),
+            "right": pygame.image.load(asset_path("assets/ui/htp_2.png"))
+        }
 
     def manage_game_events(self) -> bool:
         super().manage_game_events()
@@ -53,8 +60,7 @@ class MainMenu(scene.GameScene):
         logo_x_pos = h_width - (self.logo.get_rect().width / 2)
         self.canvas.blit(self.logo, (logo_x_pos, 128))
 
-        title_x_pos = h_width - \
-            (self.title_text.get_rect().width / 2)
+        title_x_pos = h_width - (self.title_text.get_rect().width / 2)
         self.canvas.blit(self.title_text, (title_x_pos,
                                            136 + self.logo.get_rect().height))
 
@@ -67,3 +73,13 @@ class MainMenu(scene.GameScene):
             (self.quit_button.get_rect().width / 2)
         self.quit_rect.y = quit_y_pos = h_height + 116 + self.start_rect.height
         self.canvas.blit(self.quit_button, (quit_x_pos, quit_y_pos))
+
+        htp_padding = 32
+        htp_width, htp_height = self._htp_size
+        controls_y = h_height - (htp_height / 2)
+        self.canvas.blit(
+            self.how_to_play_images["left"], (htp_padding, controls_y))
+
+        gameplay_x = window_width - htp_width - htp_padding
+        self.canvas.blit(
+            self.how_to_play_images["right"], (gameplay_x, controls_y))
